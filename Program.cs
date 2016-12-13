52a8b21c47e23bb51004a3d7c43f4b87be0a7cf1
Shiny! <imreallyshiny@gmail.com>
2016-12-13 16:04:33 +0000

    [v1.1] Added auto-updater!

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net.Sockets;
@@ -23,6 +24,90 @@ namespace CTR_V
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new CTRV());
+
+
+            #region Check for an Update
+
+            //If the user is Connected to the Internet;
+            if (TestInternetConnection())
+            {
+
+                try
+                {
+
+                    //Using WebClient, get the Newest File Version;
+                    using (System.Net.WebClient wc = new System.Net.WebClient())
+                    {
+
+                        //Latest Version;
+                        string LatestVersion = wc.DownloadString("https://raw.githubusercontent.com/initPRAGMA/CTR-V/master/version.txt");
+                        string ExecutableLocation = typeof(Program).Assembly.CodeBase.Replace("file:///", "");
+                        string CurrentVersion = FileVersionInfo.GetVersionInfo(ExecutableLocation).ProductVersion;
+                        string CurrentExecutableName = typeof(Program).Assembly.GetName().Name + "-" + LatestVersion + ".exe";
+
+                        //If the Latest Version is Newer then the Current Version;
+                        if (LatestVersion != CurrentVersion)
+                        {
+                            MessageBox.Show(CurrentExecutableName);
+                            //Download the Latest Version of the EXE file;
+                            wc.DownloadFile("https://github.com/ImReallyShiny/CTR-V/raw/master/CTR-V.exe", CurrentExecutableName);
+
+                            //Show a MessageBox asking to open Explorer to the file;
+                            DialogResult mb = MessageBox.Show("Continue usage on the new update. Open Explorer and go to the Directory containing the updated .exe located at: " + ExecutableLocation.Replace("CTR-V.EXE", CurrentExecutableName + " ?\""), "New Update Downloaded!", MessageBoxButtons.YesNo, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1);
+                            if (mb == DialogResult.Yes)
+                            {
+                                //Go to where SAIO is and select the New Update.exe;
+                                Process.Start("explorer.exe", "/select,\"" + ExecutableLocation.Replace("/", "\\").Replace("CTR-V.EXE", CurrentExecutableName) + "\"");
+                            }
+
+                        }
+                        else
+                        {
+
+                            Application.EnableVisualStyles();
+                            Application.SetCompatibleTextRenderingDefault(false);
+                            Application.Run(new CTRV());
+
+                        }
+
+                    }
+
+                }
+                catch (Exception ex)
+                {
+
+                    //Updating had an Unexpected Error;
+                    MessageBox.Show("Please re-download CTR-V manually as an Update Bug Occured! Error: " + ex.ToString(), "Update Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                }
+
+            }
+            else
+            {
+
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new CTRV());
+
+            }
+
+            #endregion
+
         }
+
+        private static bool TestInternetConnection()
+        {
+            try
+            {
+                System.Net.NetworkInformation.Ping ping = new System.Net.NetworkInformation.Ping();
+                ping.Send("google.com");
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
     }
 }
